b1392dc29ecf2b185636129f9730f70e318c5d73
Ignatius Alriana Haryadi Moel <alrianatcm@gmail.com>
2016-05-30 11:27:13 +0700

    add more test

--- App/biodata.controller.js
+++ App/biodata.controller.js
@@ -1,54 +1,59 @@
-angular.module('biodata',[])
-    .controller('inputName', inputName);
+(function () {
+    'use strict';
 
-function inputName() {
-    var vm = this;
-    vm.name = '';
-    vm.memory = [];
-    vm.age = 0;
-    vm.years = {
-        1990: '1990',
-        1991: '1991',
-        1992: '1992',
-        1993: '1993',
-        1994: '1994',
-        1995: '1995',
-        1996: '1996',
-        1997: '1997',
-        1998: '1998',
-        1999: '1999',
-        2000: '2000',
-        2001: '2001',
-        2002: '2002',
-        2003: '2003',
-        2004: '2004',
-        2005: '2005',
-        2006: '2006',
-        2007: '2007',
-        2008: '2008',
-        2009: '2009',
-        2010: '2010',
-        2011: '2011',
-        2012: '2012',
-        2013: '2013',
-        2014: '2014',
-        2015: '2015',
-        2016: '2016',
+    angular.module('biodata',[])
+        .controller('inputName', inputName);
+
+    function inputName() {
+        var vm = this;
+        vm.name = '';
+        vm.memory = [];
+        vm.age = 0;
+        vm.years = {
+            1990: '1990',
+            1991: '1991',
+            1992: '1992',
+            1993: '1993',
+            1994: '1994',
+            1995: '1995',
+            1996: '1996',
+            1997: '1997',
+            1998: '1998',
+            1999: '1999',
+            2000: '2000',
+            2001: '2001',
+            2002: '2002',
+            2003: '2003',
+            2004: '2004',
+            2005: '2005',
+            2006: '2006',
+            2007: '2007',
+            2008: '2008',
+            2009: '2009',
+            2010: '2010',
+            2011: '2011',
+            2012: '2012',
+            2013: '2013',
+            2014: '2014',
+            2015: '2015',
+            2016: '2016',
+        };
+        vm.calculateAge = calculateAge;
+        function calculateAge(){
+            if (vm.name!='' && vm.year !='') {
+                var currentYear = new Date().getFullYear();
+                vm.age = parseInt(currentYear) - parseInt(vm.year);
+
+                vm.memory.unshift({
+                    name: vm.name,
+                    birthyear: vm.year,
+                    age: vm.age
+                });
+                vm.name = '';
+                vm.year = '';
+            }
+        };
+
     };
-    vm.calculateAge = calculateAge;
-    function calculateAge(){
-        if (vm.name!='' && vm.year !='') {
-            var currentYear = new Date().getFullYear();
-            vm.age = parseInt(currentYear) - parseInt(vm.year);
+})();
 
-            vm.memory.unshift({
-                name: vm.name,
-                birthyear: vm.year,
-                age: vm.age
-            });
-            vm.name = '';
-            vm.year = '';
-        }
-    };
-
-};
--- App/biodata.controller.spec.js
+++ App/biodata.controller.spec.js
@@ -1,6 +1,41 @@
-describe('Protractor biodata', function () {
-    it('should have a title', function () {
+'use strict';
+
+describe("Biodata test", function () {
+    it("should have a title", function () {
         browser.get('http://alrianahm.github.io/Protractor/App/biodata.html');
         expect(browser.getTitle()).toEqual('Biodata');
     });
+
+    describe("memory", function () {
+
+        var name, year, button;
+        beforeEach(function () {
+            name = element(by.model("vm.name"));
+            year = element(by.model("vm.year"));
+            button = element(by.id("calculateButton"));
+        })
+
+        var memory = element.all(by.repeater("result in vm.memory"));
+        it("should start with empty memory", function () {
+            expect(memory.count()).toEqual(0);
+        });
+
+        it("should not add to memory when all field empty", function () {
+            button.click();
+            expect(memory.count()).toEqual(0);
+        });
+
+        it("should not add to memory when year empty", function () {
+            name.sendKeys("Alriana");
+            button.click();
+            expect(memory.count()).toEqual(0);
+        });
+/*
+        it("should not add to memory when name empty", function () {
+            button.click();
+            expect(memory.count()).toEqual(0);
+        });
+*/
+    })
+
 });
